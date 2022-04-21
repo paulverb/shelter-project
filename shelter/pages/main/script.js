78582714f7f52fa412cead4e369642bc0f227b3c
Pavel Verbitsky <plov235@gmail.com>
2022-04-21 18:58:52 +0200

    feat: complete burger on page main

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -4,6 +4,8 @@ const burger = document.querySelector('.burger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const body = document.querySelector('body')
 const dark = document.querySelector('.dark')
+const burgerItems = document.querySelectorAll('.burger-nav-item')
+
 
 // flags
 let isMobileOpen = false;
@@ -21,6 +23,8 @@ function openOrCloseMobileMenu(){
 function openMobile(){
   mobileMenu.classList.add('active')
   burger.classList.add('opened')
+  body.classList.add('hide-scroll')
+  
   isMobileOpen = true;
   
 }
@@ -28,19 +32,33 @@ function openMobile(){
 function closeMobile(){
   mobileMenu.classList.remove('active')
   burger.classList.remove('opened')
+  body.classList.remove('hide-scroll')
+  dark.classList.remove('darkened')
   isMobileOpen = false;
 }
 
+const disableScroll = (body) => body.classList.add('hide-scroll')
 
 // event listeners
 burger.addEventListener('click', openOrCloseMobileMenu)
+// burger.addEventListener('click', ()=> body.classList.add('hide-scroll'))
+dark.addEventListener('click', (e)=> {
+  if (e.target.classList.contains('darkened')){
+    closeMobile();
+  }
+})
 
-body.addEventListener('click', (e)=> console.log(e.target))
+
+//body.addEventListener('click', (e)=> console.log(e.target))
 //dark.classList.toggle('darkened')
 
 
 const footerPuppy = document.querySelector('.footer-puppy')
 footerPuppy.addEventListener('click', (e)=> e.target.classList.toggle('active'))
+
+burgerItems.forEach( (item)=> item.addEventListener('click', (item)=>console.log('burger' + item) ))
+burgerItems.forEach( (item)=> item.addEventListener('click', closeMobile ))
+
 
 
 
